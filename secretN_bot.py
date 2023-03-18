4417cccb03f3eecbde19883c9fc24c5374b13015
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-03-19 01:02:51 +0300

    a bit PEP8, split states and JSON correction...

--- secretN_bot.py
+++ secretN_bot.py
@@ -1,17 +1,12 @@
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.utils import executor
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-import config
 import json
+import config, states
 
 
-class Password(StatesGroup):
-    waiting_for_registration = State()
-    
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -24,50 +19,60 @@ async def command_start(message: types.Message):
     await bot.send_message(message.from_user.id, 
                            f'Приветствую, твой id={message.from_user.id}. Выбери действие')
 
+
 @dp.message_handler(commands=['help'])
 async def command_help(message: types.Message):
     await bot.send_message(message.from_user.id, 
                            f'Приветствую, {message.from_user.id}. Я умею надежно хранить твои пароли и информацию!')
     await bot.send_message(message.from_user.id, 
-                           'Введи одну из команд:\n/Регистрация: регистрирует пользователей в нашей системе\n/Мои секреты: ваша БД')
+                           'Введи одну из команд:\n/Регистрация : регистрирует пользователей в нашей системе\n/Мои секреты : ваша БД')
+
 
 @dp.message_handler(commands=['Регистрация'])
 async def registation(message: types.Message,):
     with open('passwords.json', 'r') as pw:
-        if '"' + str(message.from_user.id) + '_password"' in pw.read():
+        if str(message.from_user.id) + '_password":' in pw.read():
             await bot.send_message(message.from_user.id, 
                                 f'{message.from_user.id}, вы уже регистрировались здесь!')
         else:
             await bot.send_message(message.from_user.id, 
                                 f'{message.from_user.id}, придумайте пароль.') 
-            await Password.waiting_for_registration.set()
+            await states.Password.waiting_for_registration.set()
 
-@dp.message_handler(state=Password.waiting_for_registration)              
+
+@dp.message_handler(state=states.Password.waiting_for_registration)              
 async def validation(message: types.Message, state: FSMContext):
     password = message.text
+    hiden_password = len(message.text)*'*'
     await state.update_data(password=password)
+
     if ' ' in password:
         await bot.send_message(message.from_user.id, 
-                                f'{message.from_user.id}, пароль не должен содержать пробелы!')
+                               f'{message.from_user.id}, пароль не должен содержать пробелы!')
         await message.delete()
     else:
         password_conc = str(message.from_user.id) + '_password'
         password_to_db = {password_conc : message.text}
-        with open('passwords.json', 'w') as pw:
-            json.dump(password_to_db, pw)
-        hiden_password = len(message.text)*'*'
+
+        with open('passwords.json') as pw:
+            data = json.load(pw)
+            data['passwords'].append(password_to_db)
+            with open('passwords.json', 'w') as outpw:
+                json.dump(data, outpw)
+
         await bot.send_message(message.from_user.id, 
                                 f'{message.from_user.id}, пароль {hiden_password} успешно сохранен.')
         await message.delete()                
+    
     await state.finish()
+
 
 @dp.message_handler(commands=['Мои секреты'])
 async def command_help(message: types.Message):
     await bot.send_message(message.from_user.id, 
                            f'{message.from_user.id}, введите пароль.')
     
-#=================================================================================================
-    
+#================================================================================================= 
 @dp.message_handler()
 async def send_id(message: types.Message):
     await bot.send_message(message.from_user.id, f'Напиши /start или /help')
